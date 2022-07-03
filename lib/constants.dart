cff1a4b6ee4770021cbda703bc0799cfab39efcc
Ali <ali.baqeri.yazdabady@gmail.com>
2022-07-04 01:24:49 +0430

    1401-4-12 constants,local.properties

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,9 +3,9 @@ class Constants {
   Constants._();
 
   /// used for (app folder, send to server)
-  static const appName = 'amlak';
+  static const appName = 'vosate_zehn';
   /// used for (app title)
-  static String appTitle = 'amlak';
+  static String appTitle = 'vosate zehn';
   static String appVersionName = '0.0.1';                     //match by pubspec.yaml
   static int appVersionCode = (0 * 1000) + (0 * 100) + (1);
 }
