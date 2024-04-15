490ddb828fe3e439e6a5be72c63bb8bd2cc2ff67
ali-1989 <ali.baqeri.yazdabady@gmail.com>
2024-04-15 09:30:16 +0330

    1403-01-27, c2

---
+++ lib/structures/enums/ui_state.dart
@@ -0,0 +1,30 @@
+enum UiStates {
+  normal(1),
+  loading(2),
+  error(3),
+  noData(4);
+
+  final int _id;
+
+  const UiStates(this._id);
+
+  factory UiStates.from(dynamic data) {
+    if(data == null){
+      return UiStates.normal;
+    }
+
+    if(data is String) {
+      values.firstWhere((e) => e.name == data, orElse: ()=> UiStates.normal);
+    }
+
+    if(data is int) {
+      values.firstWhere((e) => e._id == data, orElse: ()=> UiStates.normal);
+    }
+
+    return UiStates.normal;
+  }
+
+  int id(){
+    return _id;
+  }
+}
